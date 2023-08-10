e26d4b6fe52ae978324b791e6ff8bcd3454397d7
EunbeenDev <ros4312@naver.com>
2023-08-11 06:08:22 +0900

    [Feat] #5 카카오 로그인 탈퇴하기 api 구현
    유저 정보 조회 방법: 카카오 token->email->userId

--- src/main/java/com/example/water/global/auth/KakaoController.java
+++ src/main/java/com/example/water/global/auth/KakaoController.java
@@ -1,21 +1,22 @@
 package com.example.water.global.auth;
+import com.example.water.domain.user.User;
 import com.example.water.domain.user.UserDto;
 import com.example.water.domain.user.UserService;
 import com.example.water.global.BaseResponse;
+import com.example.water.global.ErrorCode;
 import com.example.water.global.SuccessCode;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.Map;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class KakaoController {
@@ -30,6 +31,7 @@ public class KakaoController {
     @Value("${kakao.redirect.url}")
     private String REDIRECT_URL;
 
+
     @GetMapping("/login")
     public String kakaoLogin() {
         String kakaoLoginUrl = "https://kauth.kakao.com/oauth/authorize" +
@@ -38,14 +40,15 @@ public class KakaoController {
                 "&response_type=code" +
                 "&scope=profile_nickname profile_image account_email";
 
-        return "redirect:" + kakaoLoginUrl;
+        return "redirect:"+kakaoLoginUrl;
     }
 
+    @ResponseBody
     @GetMapping("/kakao")
     public ResponseEntity<BaseResponse<UserDto>> getUI(@RequestParam String code) throws IOException, ParseException {
         String default_image = "https://media.discordapp.net/attachments/1133490407649591316/1138778386492301463/-04.png?width=662&height=662";
         String access_token = kakaoService.getToken(code);
-        Map<String, Object> userInfoMap = kakaoService.getUserInfo(access_token, default_image);
+        Map<String, Object> userInfoMap = kakaoService.getUserInfo(access_token);
         UserDto userInfo = UserDto.builder()
                 .nickname((String) userInfoMap.get("nickname"))
                 .image((String) userInfoMap.get("profileImage"))
@@ -54,21 +57,42 @@ public class KakaoController {
 
         String email = userInfo.getEmail();
 
-        if (!userService.existsEmail(email)) {
-            String profileImage = (String) userInfoMap.get("profileImage");
-            if (profileImage != null) {
-                userInfo.setImage(profileImage);
-            } else {
-                userInfo.setImage(default_image);
-            }
-            userService.createUser(userInfo);
-        }
-        // BaseResponse 객체를 생성하여 JSON 응답 데이터 구성
-        BaseResponse<UserDto> response = BaseResponse.success(SuccessCode.CUSTOM_SUCCESS, userInfo);
+        User user = userService.findByEmail(email);
 
-        // ResponseEntity를 사용하여 HTTP 상태 코드와 함께 응답 데이터 반환
+        if (user == null) {
+            String profileImage = (String) userInfoMap.get("profileImage");
+            if (profileImage == null || profileImage.isEmpty()) {
+                userInfo.setImage(default_image);
+            } else {
+                userInfo.setImage(profileImage);
+            }
+            userService.createUser(userInfo);// 새로운 사용자 정보를 DB에 등록
+        }
+        BaseResponse<UserDto> response = BaseResponse.success(SuccessCode.CUSTOM_SUCCESS, userInfo);
         return ResponseEntity.status(response.getStatus()).body(response);
     }
+
+    @PostMapping("/exit")
+    public ResponseEntity<BaseResponse<String>> exitKakao(@RequestParam String access_token) {
+        try {
+            Map<String, Object> userInfo = kakaoService.getUserInfo(access_token);
+            String email = (String) userInfo.get("email");
+
+            //email을 사용하여 DB에서 유저 정보 조회
+            User user = userService.findByEmail(email);
+
+            Long userId = user.getUserId();
+            kakaoService.deleteUser(access_token, userId);
+
+            BaseResponse<String> response = BaseResponse.success(SuccessCode.CUSTOM_SUCCESS, "계정 탈퇴가 완료되었습니다.");
+            return ResponseEntity.ok(response);
+        } catch (IOException e) {
+            e.printStackTrace();
+            BaseResponse<String> errorResponse = BaseResponse.error(ErrorCode.REQUEST_VALIDATION_EXCEPTION, "계정 탈퇴에 실패하였습니다.");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        }
+    }
+
 }
 
 
