6f95492ef3a24838289c2460a3548317053b3b92
EunbeenDev <ros4312@naver.com>
2023-08-13 07:30:13 +0900

    [Fix] #16 응답값 출력 문제 해결

--- src/main/java/com/example/water/domain/comment/CommentService.java
+++ src/main/java/com/example/water/domain/comment/CommentService.java
@@ -1,6 +1,5 @@
 package com.example.water.domain.comment;
 
-import com.example.water.domain.crystal.Crystal;
 import com.example.water.domain.crystal.CrystalRepository;
 import com.example.water.domain.user.User;
 import com.example.water.domain.user.UserRepository;
@@ -14,33 +13,23 @@ import java.util.*;
 @RequiredArgsConstructor
 public class CommentService {
     private final UserRepository userRepository;
-    private final CrystalRepository crystalRepository;
-    private final CommentRepository commentRepository;
 
     public List<Map<String, Object>> getCommentResponses(Long userId, Long crystalId) {
-        Optional<User> userOptional = userRepository.findById(userId);
+        Optional<User> user = userRepository.findById(userId);
 
-        User user = userOptional.get();
-
-        List<Comment> comments = new ArrayList<>();
-
-        // User의 Crystals에서 해당하는 Crystal을 찾는 부분 추가
-        for (Crystal crystal : user.getCrystals()) {
-            if (crystal.getCrystalId().equals(crystalId)) {
-                comments = crystal.getComments();
-                break;
-            }
-        }
-
+        List<Comment> comments = user.get().getComments();
         List<Map<String, Object>> commentResponses = new ArrayList<>();
 
         for (Comment comment : comments) {
-            Map<String, Object> response = new HashMap<>();
-            response.put("user_id", comment.getUserId());
-            response.put("crystal_id", comment.getCrystalId());
-            response.put("content", comment.getContent());
-            response.put("date", comment.getDate());
-            commentResponses.add(response);
+            if (comment.getCrystalId().getCrystalId().equals(crystalId)) {
+                Map<String, Object> response = new HashMap<>();
+                response.put("diary_id",comment.getDiaryId());
+                response.put("user_id", comment.getUserId().getUserId());
+                response.put("crystal_id", comment.getCrystalId().getCrystalId());
+                response.put("content", comment.getContent());
+                response.put("date", comment.getDate());
+                commentResponses.add(response);
+            }
         }
         return commentResponses;
     }
