66412396982f37f5f14e939ce275f2978bc8eba8
bluesun147 <bluesun147@naver.com>
2023-08-13 19:35:07 +0900

    [Fix] DataRequest, DataResponse 서로 바뀜

--- src/main/java/com/example/water/domain/diary/dto/request/DiaryRequest.java
+++ src/main/java/com/example/water/domain/diary/dto/request/DiaryRequest.java
@@ -1,25 +1,8 @@
 package com.example.water.domain.diary.dto.request;
 
-import lombok.Builder;
 import lombok.Data;
-
-import java.time.LocalDateTime;
 
 @Data
 public class DiaryRequest {
     private String diary;
-    private LocalDateTime localDateTime;
-
-    @Builder
-    public DiaryRequest(String diary, LocalDateTime localDateTime) {
-        this.diary = diary;
-        this.localDateTime = localDateTime;
-    }
-
-    public static DiaryRequest of(String diary, LocalDateTime localDateTime) {
-        return DiaryRequest.builder()
-                .diary(diary)
-                .localDateTime(localDateTime)
-                .build();
-    }
 }
--- src/main/java/com/example/water/domain/diary/dto/response/DiaryResponse.java
+++ src/main/java/com/example/water/domain/diary/dto/response/DiaryResponse.java
@@ -1,8 +1,31 @@
 package com.example.water.domain.diary.dto.response;
 
+import lombok.Builder;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 public class DiaryResponse {
     private String diary;
+    private LocalDateTime localDateTime;
+    private Long emotionId;
+    private Long myCrystalCount;
+
+    @Builder
+    public DiaryResponse(String diary, LocalDateTime localDateTime, Long emotionId, Long myCrystalCount) {
+        this.diary = diary;
+        this.localDateTime = localDateTime;
+        this.emotionId = emotionId;
+        this.myCrystalCount = myCrystalCount;
+    }
+
+    public static DiaryResponse of(String diary, LocalDateTime localDateTime, Long emotionId, Long myCrystalCount) {
+        return DiaryResponse.builder()
+                .diary(diary)
+                .localDateTime(localDateTime)
+                .emotionId(emotionId)
+                .myCrystalCount(myCrystalCount)
+                .build();
+    }
 }
